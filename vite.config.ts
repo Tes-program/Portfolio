d4f6c7fb1cfcbddfb5fc267fcacfdb290e3c9334
Tes-program <teslimodumuyiwa@gmail.com>
2025-07-30 23:03:37 +0100

    [FEAT]: Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Portfolio/', // Add this line - must match your repo name exactly
   server: {
     host: "::",
     port: 8080,
